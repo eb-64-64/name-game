079c2769cdf6091a2199eeea6b54b7f1ed8641e5
eb-64-64 <53383020+eb-64-64@users.noreply.github.com>
2025-07-15 18:53:09 -0600

    add two pages

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
+  import { Router, type RouteConfig } from "@mateothegreat/svelte5-router";
+    import Player from "./lib/Player.svelte";
+    import Display from "./lib/Display.svelte";
+
+  const routes: RouteConfig[] = [
+    {
+      component: Player,
+    },
+    {
+      path: "display",
+      component: Display,
+    },
+  ];
 </script>
 
-Hello world!
+<Router {routes} />
